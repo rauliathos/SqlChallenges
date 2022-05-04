40e74e9d2b967c9bc751ec127c018d45b3f3adaa
Raul Ciobanu <44482086+rauliathos@users.noreply.github.com>
2022-05-04 14:55:18 +0100

    2-5

--- SakilaChallenge.sql
+++ SakilaChallenge.sql
@@ -1,2 +1,14 @@
 --1. List all actors
 SELECT * from actors;
+
+-- 2 FInd the surname of the actors with the forname 'John'
+SELECT last_name from actor where first_name = 'John';
+
+-- 3 Find all actors with surname 'Neeson'.
+select * from actor where last_name = 'Neeson';
+
+--4 Find all actors with ID numbers divisible by 10.
+select * from actor where (actor_id %10) = 0;
+
+--5What is the description of the movie with an ID of 100?
+select description from film where film_id = 100;
